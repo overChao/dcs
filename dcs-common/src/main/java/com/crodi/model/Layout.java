4f6dd8446585126025a7c58f1385fcae5be54a8d
zhangkc <Crodis_Die@163.com>
2026-07-15 13:54:42 +0800

    init project

---
+++ dcs-common/src/main/java/com/crodi/model/Layout.java
@@ -0,0 +1,15 @@
+package com.crodi.model;
+
+/**
+ * @Author: crodi.zhang
+ * @Date: 2026/7/15 11:15
+ * @Description: TODO
+ **/
+
+public interface Layout {
+
+    // 图层
+    String getLayer();
+
+
+}
---
+++ dcs-common/src/main/java/com/crodi/model/Node.java
@@ -0,0 +1,20 @@
+package com.crodi.model;
+
+import lombok.Data;
+
+/**
+ * @Author: crodi.zhang
+ * @Date: 2026/7/15 11:17
+ * @Description: TODO
+ **/
+
+@Data
+public class Node {
+
+
+    private String nodeCode;
+
+
+
+
+}
